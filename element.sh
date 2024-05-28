471d21d9e9f847918cb996be26c6ed27cd2bcc90
Nebal Maysaud <nmaysaud@gmail.com>
2024-05-28 16:33:23 +0000

    feat: add name or symbol as argument

--- element.sh
+++ element.sh
@@ -52,4 +52,48 @@ then
   echo -e "The element with atomic number $ATOMIC_NO is $ELEMENT_NAME. It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $ELEMENT_MELTING_POINT celsius and a boiling point of $ELEMENT_BOILING_POINT celsius."
 
   fi
+
+# if $1 is not either an element name, or a symbol
+elif [[ -z $(echo -e "$($PSQL "
+    SELECT atomic_number FROM elements WHERE name='$1' OR symbol='$1'
+  ")") ]]
+
+then
+  #return message
+  NOT_FOUND
+
+else
+  #get atomic no
+  ATOMIC_NO=$($PSQL "
+    SELECT atomic_number FROM elements WHERE name='$1' OR symbol='$1'
+  ")
+
+    # get element name
+    ELEMENT_NAME=$(echo -e "$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NO")")
+
+  # get element type
+    ELEMENT_TYPE=$(echo -e "$($PSQL "
+      SELECT types.type FROM types 
+      INNER JOIN properties USING(type_id)
+      WHERE atomic_number=$ATOMIC_NO
+    ")")
+
+  # get atomic mass
+    ATOMIC_MASS=$(echo -e "$($PSQL "
+      SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NO
+    ")")
+
+  # get element melting point 
+    ELEMENT_MELTING_POINT=$(echo -e "$($PSQL "
+      SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NO
+    ")")
+
+  # get element boiling point
+    ELEMENT_BOILING_POINT=$(echo -e "$($PSQL "
+      SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NO
+    ")")
+
+  # output
+  echo -e "The element with atomic number $ATOMIC_NO is $ELEMENT_NAME. It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $ELEMENT_MELTING_POINT celsius and a boiling point of $ELEMENT_BOILING_POINT celsius."
+
 fi
